4925d42832004069432df3ccf84ce533b783b7c0
Mokaku <mokaku@gmail.com>
2014-04-24 12:05:14 +0900

    debug set_default_bash.sh to add Escape code to Valiables

--- set_default_bash.sh
+++ set_default_bash.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -ex
 
-cat <<__EOA__ >>~/.login
+cat <<__EOA__ >~/.login
 # .login
 #
 # ログインシェルがcsh/tcshだった場合に好きなシェルを起動する。
 # chshできるならした方が良いが、chshできない環境で重宝する。
 set favorit_shell=/bin/bash
 set favorit_shell_opt=--login
-if ( -f "$favorit_shell" && "$shell" != "$favorit_shell" ) then
-  exec $favorit_shell $favorit_shell_opt
+if ( -f "\$favorit_shell" && "\$shell" != "\$favorit_shell" ) then
+  exec \$favorit_shell \$favorit_shell_opt
 endif
 __EOA__
 
-cat <<__EOB__ >>~/.profile
+cat <<__EOB__ >~/.profile
 # .profile
 #
 # ログインシェルがshだった場合に好きなシェルを起動する。
@@ -21,7 +21,7 @@ cat <<__EOB__ >>~/.profile
 # chshできるならした方が良いが、chshできない環境で重宝する。
 FAVORIT_SHELL=/bin/bash
 ## FAVORIT_SHELL_OPT=--login
-if [ -e $FAVORIT_SHELL -a "$SHELL" != "$FAVORIT_SHELL" ] ; then
-  exec $FAVORIT_SHELL $FAVORIT_SHELL_OPT
+if [ -e \$FAVORIT_SHELL -a "\$SHELL" != "\$FAVORIT_SHELL" ] ; then
+  exec \$FAVORIT_SHELL \$FAVORIT_SHELL_OPT
 fi
 __EOB__
